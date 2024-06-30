7b399f6a8e68d69466a0d4c9b8cad37be2940b86
hpham474 <hpham474@gmail.com>
2024-06-30 03:44:40 -0500

    create search bar to search for city and log to console

--- src/index.js
+++ src/index.js
@@ -5,20 +5,22 @@ import processForecast from "./json/processForecast";
 const API = "7799c6fbb2534aaa8dc73800242806";
 const ForecastDays = 3;
 const defaultCity = "dallas";
+const btn = document.querySelector("button");
+const search = document.querySelector("input");
 
 async function getWeather(city) {
   try {
-  const response = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=${API}&q=${city}&days=${ForecastDays}`
-  );
-  if (response.status !== 200) {
-    const errorData = await response.json();
-    printError(errorData);
-    throw new Error(errorData);
-  }
-  const data = await response.json();
-  console.log(data);
-  return processJson(data);
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=${API}&q=${city}&days=${ForecastDays}`
+    );
+    if (response.status !== 200) {
+      const errorData = await response.json();
+      printError(errorData);
+      throw new Error(errorData);
+    }
+    const data = await response.json();
+    console.log(data);
+    return processJson(data);
   } catch (err) {
     console.error(`${err}`);
   }
@@ -27,10 +29,10 @@ async function getWeather(city) {
 function processJson(data) {
   const today = processToday(data);
   const forecast = [];
-  
+
   data.forecast.forecastday.forEach((days) => {
     forecast.push(processForecast(days));
-  })
+  });
 
   const weatherData = {
     today: today,
@@ -46,10 +48,19 @@ function printError(response) {
   );
 }
 
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  let city = defaultCity;
+  if (search.value !== "") {
+    city = search.value;
+  }
+  getWeather(city);
+  search.value = "";
+});
+
 const weather = await getWeather(defaultCity);
 
 console.log(weather);
 //weather.today.print();
 //weather.forecast[0].print();
 //weather.forecast[0].getHourly(0).print();
-
